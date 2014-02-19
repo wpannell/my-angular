7adf77a8a647edb4e85f6c034a30bd103f8cbc14
wpannell <wil.pannell@gmail.com>
2014-02-19 05:44:25 -0800

    now supports jasmine

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,17 +2,35 @@ module.exports = function(grunt) {
   //noinspection JSUnresolvedFunction
   grunt.initConfig({
     jshint:{
-      all: ['src/**/*.js'],
+      all: ['src/**/*.js', 'specs/**/*.js'],
       options: {
         globals: {
-          _: false,
-          $: false
+          _: false, //lodash
+          $: false, //jquery
+
+          //jasmine
+          jasmine: false,
+          describe: false,
+          it: false,
+          expect: false,
+          beforeEach: false,
+          waits: false,
+          runs: false
         },
         browser: true,
         devel: true
       }
+    },
+    jasmine:{
+      unit:{
+        src: 'src/**/*.js',
+        options:{
+          specs: ['specs/**/*.js']
+        }
+      }
     }
-  });
+    });
 
   grunt.loadNpmTasks('grunt-contrib-jshint') ;
+  grunt.loadNpmTasks('grunt-contrib-jasmine') ;
 };
